5bb1847f66f4d68d96aaf85ca8326a7a8ab6b3da
TomAshTee <jaeschke.tomasz@gmail.com>
2018-02-11 11:09:47 +0100

    Complet VC structure

--- SpendBook/Controller/MainVC.swift
+++ SpendBook/Controller/MainVC.swift
@@ -23,15 +23,7 @@ class MainVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        /*
-        addBtn.layer.cornerRadius = addBtn.frame.size.height / 2
-        addBtn.layer.masksToBounds = true
-        addBtn.setGradientBackground(colorOne: #colorLiteral(red: 0.09411764706, green: 0.4412631798, blue: 0.9411764706, alpha: 1), colorTwo: #colorLiteral(red: 0.4274509804, green: 0.9210339206, blue: 0.9529411765, alpha: 1))
         
-        withdrawBtn.layer.cornerRadius = addBtn.frame.size.height / 2
-        withdrawBtn.layer.masksToBounds = true
-        withdrawBtn.setGradientBackground(colorOne: #colorLiteral(red: 0.9450980392, green: 0.2372768683, blue: 0.7148683176, alpha: 1), colorTwo: #colorLiteral(red: 1, green: 0.6078431373, blue: 0.7843137255, alpha: 1))
-        */
 
         // Do any additional setup after loading the view.
     }
---
+++ SpendBook/Controller/SettingsVC.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsVC.swift
+//  SpendBook
+//
+//  Created by Tomasz Jaeschke on 11.02.2018.
+//  Copyright © 2018 Tomasz Jaeschke. All rights reserved.
+//
+
+import UIKit
+
+class SettingsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
