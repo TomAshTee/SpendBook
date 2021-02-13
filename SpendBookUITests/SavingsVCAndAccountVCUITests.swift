a207d1f3f9724c28c0616d5c4a344a994e70ba80
Tomasz Jaeschke <jaeschke.tomasz@gmail.com>
2021-02-13 20:55:07 +0100

    Continuation of writing tests for Savings and Account VC

--- SpendBookUITests/SavingsVCAndAccountVCUITests.swift
+++ SpendBookUITests/SavingsVCAndAccountVCUITests.swift
@@ -37,19 +37,35 @@ class SavingsVCAndAccountVCUITests: XCTestCase {
     }
 
     func testAddNewAccount() throws {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
         
         app.launch()
-        
-        XCUIApplication().tabBars["Tab Bar"].buttons["Savings"].tap()
-        
+        app.tabBars["Tab Bar"].buttons["Savings"].tap()
         app/*@START_MENU_TOKEN@*/.staticTexts["Manage"]/*[[".buttons[\"Manage\"].staticTexts[\"Manage\"]",".staticTexts[\"Manage\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         app/*@START_MENU_TOKEN@*/.staticTexts["+Add acct."]/*[[".buttons[\"+Add acct.\"].staticTexts[\"+Add acct.\"]",".staticTexts[\"+Add acct.\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         app.alerts["Add new Account"].scrollViews.otherElements.collectionViews/*@START_MENU_TOKEN@*/.textFields["Account name"]/*[[".cells.textFields[\"Account name\"]",".textFields[\"Account name\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.typeText("Test")
         app.alerts["Add new Account"].scrollViews.otherElements.buttons["Save"].tap()
-        
-        
     }
+    
+    func testAddPartToAccount() throws {
+        
+        app.launch()
+        app.tabBars["Tab Bar"].buttons["Savings"].tap()
+        app/*@START_MENU_TOKEN@*/.staticTexts["Manage"]/*[[".buttons[\"Manage\"].staticTexts[\"Manage\"]",".staticTexts[\"Manage\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.pickerWheels.firstMatch.adjust(toPickerWheelValue: "Test")
+        app.buttons["Icon plus"].tap()
+        app.alerts["Operation"].scrollViews.otherElements.collectionViews/*@START_MENU_TOKEN@*/.textFields["Operation value"]/*[[".cells.textFields[\"Operation value\"]",".textFields[\"Operation value\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.typeText("10")
+        app.alerts["Operation"].scrollViews.otherElements.buttons["Add"].tap()
+    }
+    
+    func testDeleteAccount() throws {
+        
+        app.launch()
+        app.tabBars["Tab Bar"].buttons["Savings"].tap()
+        
+        let tablesQuery = XCUIApplication().tables
+        tablesQuery.cells.containing(.staticText, identifier: "Test").firstMatch.swipeLeft()
+        tablesQuery.buttons["DELETE"].tap()
 
+        app.alerts["Delete"].scrollViews.otherElements.buttons["Yes"].tap()
+    }
 }
